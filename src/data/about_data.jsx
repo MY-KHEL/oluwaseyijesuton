4707b69b1e60e284aac922293fb46043ca0ea65c
Michael <ojomichael965@gmail.com>
2025-02-05 22:43:31 +0100

    change

--- src/data/about_data.jsx
+++ src/data/about_data.jsx
@@ -7,7 +7,7 @@ export const about = [
         buttonText: "WORK WITH OLUWASEYI",
         hasSecondButton: false,
         secondButtonText: '',
-        imageLink:'/src/assets/image7.jpg',
+        imageLink:'/src/assets/image7.LPG',
     },
     {
         id: 2,
@@ -17,7 +17,7 @@ export const about = [
         buttonText: "READ MORE",
         hasSecondButton: false,
         secondButtonText: '',
-        imageLink:'/src/assets/image1.jpg',
+        imageLink:'/src/assets/image1.JPG',
     },
     {
         id: 3,
@@ -27,6 +27,6 @@ export const about = [
         buttonText: "ACCESS TRAINING AND RESOURCES",
         hasSecondButton:true,
         secondButtonText: 'BOOK 1 ON 1 COACHING',
-        imageLink:'/src/assets/image6.jpg',
+        imageLink:'/src/assets/image6.JPG',
     },
 ]
